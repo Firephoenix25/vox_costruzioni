57cea9ec4550988778dcafa312680eec558d3706
Kayo <kayodionizio@gmail.com>
2022-06-08 12:01:15 +0200

    Lavori New Layout and home fix space

--- src/components/slideshow.svelte
+++ src/components/slideshow.svelte
@@ -2,6 +2,8 @@
   import { fade } from "svelte/transition";
 
   export let carouselPhotos;
+  export let text;
+  export let mod;
 
   let index = 0;
 
@@ -12,9 +14,18 @@
   // <Slideshow carouselPhotos={[image1,image2]} client:load />
 </script>
 
-<img
-  transition:fade
-  alt="gallery"
-  class="absolute inset-0 w-full h-full object-cover object-center"
-  src={carouselPhotos[index]}
-/>
+<div class="box flex relative {mod}">
+  <img
+    transition:fade
+    alt="gallery"
+    class="absolute inset-0 w-full h-full object-cover object-center"
+    src={carouselPhotos[index]}
+  />
+  <div
+    class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100 flex items-center justify-center"
+  >
+    <h1 class="title-font text-4xl font-medium text-gray-900 mb-3 text-center">
+      {text}
+    </h1>
+  </div>
+</div>
